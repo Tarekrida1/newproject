a7c2b913dfd75325dfcad316da7a44a7b9267e31
TaReK RiDa <37909439+Tarekrida1@users.noreply.github.com>
2018-04-18 22:03:49 +0200

    updated

--- js/plugin.js
+++ js/plugin.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 $(function(){
     'use strict';
-
-   $(window).load(function() {  
-    $(".loader , #loading h2").slideUp(500, function(){
-        $("#loading").fadeOut(500);
-    });
-});
   var typed3 = new Typed('#ppp', {
     strings: ['I have 3 years of professional experience in developing websites based on webstandard technologies like HTML5, CSS, jquery, bootstrap, sass.'],
     typeSpeed: 20,
@@ -37,18 +30,14 @@ cursorwidth:"16px"
    $(window).scroll(function() { // check if scroll event happened
         if ($(document).scrollTop() > 50) { // check if user scrolled more than 50 from top of the browser window
       
-           
-             $(".navbar").css({
+            $(".navbar").css({
     background: "linear-gradient(to right,  #81ee8e , #00c9fd)" 
-                
 });
-     
             // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
         } else {
                       $(".navbar").css({
     background: "none" 
 });
-  
       // if not, change it back to transparent
         }
       });
@@ -61,70 +50,4 @@ cursorwidth:"16px"
     $('.navbar-nav>li>a').on('click', function(){
     $('.navbar-collapse').collapse('hide');
 });
-    
-=======
-$(function(){
-    'use strict';
-
-   $(window).load(function() {  
-    $(".loader , #loading h2").slideUp(500, function(){
-        $("#loading").fadeOut(500);
-    });
-});
-  var typed3 = new Typed('#ppp', {
-    strings: ['I have 3 years of professional experience in developing websites based on webstandard technologies like HTML5, CSS, jquery, bootstrap, sass.'],
-    typeSpeed: 20,
-    backSpeed: 5,
-    loop: true
-  });
-    var  winH = $(window).height(),
-      unavh = $('.navbar').height();
-    $('.more').height(winH - ( unavh));
-    
-    
-     $("#know-more").click(function () {
-        $("html, body").animate({
-            scrollTop: $(".more").offset().top
-        }, 1000);
-          
-    });
-      $(".more .con button").click(function () {
-        $("html, body").animate({
-            scrollTop: $(".services").offset().top
-        }, 1000);
-    });
-    
-    $("body").niceScroll({
-cursorcolor:"#81ee8e",
-cursorwidth:"16px"
-});
-   $(window).scroll(function() { // check if scroll event happened
-        if ($(document).scrollTop() > 50) { // check if user scrolled more than 50 from top of the browser window
-      
-           
-             $(".navbar").css({
-    background: "linear-gradient(to right,  #81ee8e , #00c9fd)" 
-                
-});
-     
-            // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
-        } else {
-                      $(".navbar").css({
-    background: "none" 
-});
-  
-      // if not, change it back to transparent
-        }
-      });
-    
-          $("#Templates").click(function () {
-        $("html, body").animate({
-            scrollTop: $(".bbb").offset().top
-        }, 1000);
-    });
-    $('.navbar-nav>li>a').on('click', function(){
-    $('.navbar-collapse').collapse('hide');
-});
-    
->>>>>>> c9a3d0a3f3f306000201087ebfd87784391bee52
 });
