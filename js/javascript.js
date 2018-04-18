06c8ef4f983993c3acfc6423fbd62ede6a59281b
TaReK RiDa <37909439+Tarekrida1@users.noreply.github.com>
2018-04-18 22:19:31 +0200

    s

--- js/javascript.js
+++
@@ -1,5 +0,0 @@
-/* window.addEventListener("load", function(){
-    var load_screen= document.getElementById("loading");
-    this.document.body.removeChild(load_screen);
-});
-*/
--- js/main.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    'use strict';
-  var  winH = $(window).height(),
-        upperh = $('.upper-bar').innerHeight(),
-      unavh = $('.navbar').innerHeight();
-    $('.slider').hieght(winH - (upperh + unavh));
-});
