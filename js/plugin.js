e798f3c0ff765639ada805fa9e2b5538a95e076b
TaReK RiDa <37909439+Tarekrida1@users.noreply.github.com>
2018-04-19 14:26:03 +0200

    up

--- js/plugin.js
+++ js/plugin.js
@@ -1,7 +1,42 @@
 $(function(){
     'use strict';
-  
-  
+  // fixed option
+  $(".fixed-option .fa-sliders-h").on('click', function () {
+    $(this).parent(".fixed-option").toggleClass("is-visible");
+    if ($(this).parent(".fixed-option").hasClass("is-visible")) {
+
+        $(this).parent(".fixed-option").animate({
+            left: 0
+        }, 500);
+        
+        $("body").animate({
+            paddingLeft: "240px"
+        }, 500);
+
+
+    
+
+    } else {
+        $(this).parent(".fixed-option").animate({
+            left: "-240"
+        }, 500); 
+          
+        $("body").animate({
+            paddingLeft: 0
+        }, 500);
+
+    }
+  });
+
+ 
+
+    $(".navbar li a").click(function (e) {
+        e.preventDefault();
+        $("html, body").animate({
+        scrollTop: $("#" + $(this).data("scroll")).offset().top
+        }, 1000);
+    });
+    
   var typed3 = new Typed('#ppp', {
     strings: ['I have 3 years of professional experience in developing websites based on webstandard technologies like HTML5, CSS, jquery, bootstrap, sass.'],
     typeSpeed: 20,
